81198722e96df60adf2cd0839921e735d9ca5eeb
alllexey <makarovalesha0212@gmail.com>
2025-09-29 20:03:52 +0300

    Allow creating different handlers for different scopes

--- src/main/kotlin/me/alllexey123/itmoqueue/bot/command/CommandHandler.kt
+++ src/main/kotlin/me/alllexey123/itmoqueue/bot/command/CommandHandler.kt
@@ -2,10 +2,6 @@ package me.alllexey123.itmoqueue.bot.command
 
 import me.alllexey123.itmoqueue.bot.MessageContext
 import me.alllexey123.itmoqueue.bot.Scope
-import me.alllexey123.itmoqueue.bot.extensions.withReplyTo
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage
-import org.telegram.telegrambots.meta.api.objects.message.Message
-import org.telegram.telegrambots.meta.generics.TelegramClient
 
 interface CommandHandler {
 
@@ -14,20 +10,4 @@ interface CommandHandler {
     fun command(): String
 
     fun scope(): Scope
-
-    fun groupChatOnlyError(client: TelegramClient, message: Message) {
-        client.execute(
-            SendMessage.builder()
-                .text("Эта команда доступна только для групп")
-                .withReplyTo(message)
-        )
-    }
-
-    fun userChatOnlyError(client: TelegramClient, message: Message) {
-        client.execute(
-            SendMessage.builder()
-                .text("Эта команда доступна только для ЛС")
-                .withReplyTo(message)
-        )
-    }
 }
--- src/main/kotlin/me/alllexey123/itmoqueue/bot/command/CommandManager.kt
+++ src/main/kotlin/me/alllexey123/itmoqueue/bot/command/CommandManager.kt
@@ -4,10 +4,13 @@ import jakarta.annotation.PostConstruct
 import jakarta.transaction.Transactional
 import me.alllexey123.itmoqueue.bot.MessageContext
 import me.alllexey123.itmoqueue.bot.Scope
+import me.alllexey123.itmoqueue.bot.extensions.withReplyTo
 import me.alllexey123.itmoqueue.services.ContextService
 import me.alllexey123.itmoqueue.services.Telegram
 import org.springframework.stereotype.Component
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.message.Message
+import org.telegram.telegrambots.meta.generics.TelegramClient
 
 @Component
 class CommandManager(
@@ -41,32 +44,41 @@ class CommandManager(
     fun handleCommand(message: Message) {
         val text = message.text
         val command = text.split("@", limit = 2)[0].split(" ", limit = 2)[0].substring(1)
-        handlers.forEach { handler ->
-            handler.run {
-                if (command().equals(command, ignoreCase = true)) {
-                    val membership = contextService.getMembership(message)
-                    val user = membership?.user
-                        ?: contextService.getUser(message)
+        val commandNameMatches = handlers.filter { it.command().equals(command, ignoreCase = true) }
+        val currentScope = if (message.isUserMessage) Scope.USER else Scope.GROUP
+        val fullMatches = commandNameMatches.filter { it.scope() == Scope.ANY || it.scope() == currentScope }
+        if (fullMatches.isEmpty() && commandNameMatches.isNotEmpty()) {
+            if (currentScope == Scope.USER) groupChatOnlyError(telegram, message)
+            if (currentScope == Scope.GROUP) userChatOnlyError(telegram, message)
+        } else {
+            val membership = contextService.getMembership(message)
+            val user = membership?.user
+                ?: contextService.getUser(message)
 
-                    val context = MessageContext(
-                        message = message,
-                        user = user,
-                        membership = membership,
-                    )
-
-                    if (scope() == Scope.ANY) {
-                        handle(context)
-                    } else {
-                        val isUserChat = message.isUserMessage
-                        if (scope() == Scope.USER) {
-                            if (isUserChat) handle(context) else userChatOnlyError(telegram, message)
-                        }
-                        if (scope() == Scope.GROUP) {
-                            if (!isUserChat) handle(context) else groupChatOnlyError(telegram, message)
-                        }
-                    }
-                }
+            val context = MessageContext(
+                message = message,
+                user = user,
+                membership = membership,
+            )
+            fullMatches.forEach {
+                it.handle(context)
             }
         }
+    }
+
+    fun groupChatOnlyError(client: TelegramClient, message: Message) {
+        client.execute(
+            SendMessage.builder()
+                .text("Эта команда доступна только для групп")
+                .withReplyTo(message)
+        )
+    }
+
+    fun userChatOnlyError(client: TelegramClient, message: Message) {
+        client.execute(
+            SendMessage.builder()
+                .text("Эта команда доступна только для ЛС")
+                .withReplyTo(message)
+        )
     }
 }
