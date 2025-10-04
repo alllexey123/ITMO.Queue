732a75ede6ecd95a102978efa51dfab2671c2ce7
alllexey <makarovalesha0212@gmail.com>
2025-10-05 02:00:25 +0300

    Add /start keyboard

--- src/main/kotlin/me/alllexey123/itmoqueue/bot/command/StartCommand.kt
+++ src/main/kotlin/me/alllexey123/itmoqueue/bot/command/StartCommand.kt
@@ -5,6 +5,8 @@ import me.alllexey123.itmoqueue.bot.Scope
 import me.alllexey123.itmoqueue.services.Telegram
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.methods.ParseMode
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow
 
 @Component
 class StartCommand(private val telegram: Telegram) : CommandHandler {
@@ -17,9 +19,17 @@ class StartCommand(private val telegram: Telegram) : CommandHandler {
                 Я - бот для создания очередей на сдачу лабораторных работ в ИТМО.
                 Рекомендую прочитать инструкцию [тут](https://telegra.ph/Instrukciya-dlya-bota-ITMOQueue-10-04)
             """.trimIndent())
+            .replyMarkup(buildKeyboard())
             .parseMode(ParseMode.MARKDOWN)
             .build()
         telegram.execute(send)
+    }
+
+    fun buildKeyboard(): ReplyKeyboardMarkup {
+        return ReplyKeyboardMarkup.builder()
+            .keyboardRow(KeyboardRow("/list_labs", "/set_name"))
+            .resizeKeyboard(true)
+            .build()
     }
 
     override fun command() = "start"
