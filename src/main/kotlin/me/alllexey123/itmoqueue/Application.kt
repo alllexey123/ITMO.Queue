348a9f505f56e96048a2ae2b1e00986f2a8ab40a
alllexey <alllexey.dev@gmail.com>
2025-10-07 13:36:30 +0300

    Move group name refreshing

--- src/main/kotlin/me/alllexey123/itmoqueue/Application.kt
+++ src/main/kotlin/me/alllexey123/itmoqueue/Application.kt
@@ -3,20 +3,17 @@ package me.alllexey123.itmoqueue
 import jakarta.annotation.PostConstruct
 import me.alllexey123.itmoqueue.bot.TelegramBot
 import me.alllexey123.itmoqueue.services.GroupService
-import me.alllexey123.itmoqueue.services.Telegram
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication
-import org.telegram.telegrambots.meta.api.methods.groupadministration.GetChat
 
 @SpringBootApplication
 @EnableJpaAuditing
 class Application(
 	private val telegramBot: TelegramBot,
 	private val itmoQueueProperties: ItmoQueueProperties,
-	private val groupService: GroupService,
-	private val telegram: Telegram
+	private val groupService: GroupService
 ) {
 	lateinit var telegramApp: TelegramBotsLongPollingApplication
 
@@ -25,15 +22,7 @@ class Application(
 		telegramApp = TelegramBotsLongPollingApplication()
 		telegramApp.registerBot(itmoQueueProperties.botToken, telegramBot)
 
-		groupService.findAll()
-			.forEach { group -> {
-				try {
-					val getGroupInfo = GetChat.builder().chatId(group.chatId).build()
-					val res = telegram.execute(getGroupInfo)
-					group.name = res.title
-				} catch (e: Exception) {
-				}
-			} }
+		groupService.refreshGroupNames()
 	}
 
 }
--- src/main/kotlin/me/alllexey123/itmoqueue/services/GroupService.kt
+++ src/main/kotlin/me/alllexey123/itmoqueue/services/GroupService.kt
@@ -1,12 +1,14 @@
 package me.alllexey123.itmoqueue.services
 
+import jakarta.transaction.Transactional
 import me.alllexey123.itmoqueue.model.Group
 import me.alllexey123.itmoqueue.repositories.GroupRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.telegram.telegrambots.meta.api.methods.groupadministration.GetChat
 
 @Service
-class GroupService(private val groupRepository: GroupRepository) {
+class GroupService(private val groupRepository: GroupRepository, private val telegram: Telegram) {
 
     fun findById(groupId: Long): Group? {
         return groupRepository.findByIdOrNull(groupId)
@@ -19,19 +21,35 @@ class GroupService(private val groupRepository: GroupRepository) {
     fun save(group: Group): Group {
         return groupRepository.save(group)
     }
-    
+
     fun findByChatId(chatId: Long): Group? {
         return groupRepository.findByChatId(chatId)
     }
-    
+
     fun getOrCreateByChatId(chatId: Long): Group {
         val group = findByChatId(chatId)
         if (group == null) {
-            return save(Group(
-                name = null,
-                chatId = chatId
-            ))
+            return save(
+                Group(
+                    name = null,
+                    chatId = chatId
+                )
+            )
         }
         return group
+    }
+
+    @Transactional
+    fun refreshGroupNames() {
+        findAll().forEach { group ->
+                {
+                    try {
+                        val getGroupInfo = GetChat.builder().chatId(group.chatId).build()
+                        val res = telegram.execute(getGroupInfo)
+                        group.name = res.title
+                    } catch (e: Exception) {
+                    }
+                }
+            }
     }
 }
